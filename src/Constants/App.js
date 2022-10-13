3d4c091f9a04e5029ad94e33e9ef13389c9c2f2c
Ejaaz Lakhani <60117208+EjaazLakhani1@users.noreply.github.com>
2022-10-12 22:19:09 -0600

    test (#3)

---
+++ src/Constants/App.js
@@ -0,0 +1,34 @@
+import logo from '../logo.svg'
+
+export const SECTIONS = {
+  'Ejaaz': {
+    'id': 1,
+    'topic': 'Password Cracking',
+    'author': 'Ejaaz Lakhani',
+    'image': logo,
+  },
+  'Habiba': {
+    'id': 2,
+    'topic': 'Social Engineering',
+    'author': 'Habiba',
+    'image': logo,
+  },
+  'Dante': {
+    'id': 3,
+    'topic': 'Reverse Engineering',
+    'author': 'Dante',
+    'image': logo,
+  },
+  'Colton': {
+    'id': 4,
+    'topic': 'How to protect yourself on the internet',
+    'author': 'Colton',
+    'image': logo,
+  },
+  'Auric': {
+    'id': 5,
+    'topic': 'Web Exploits',
+    'author': 'Colton',
+    'image': logo,
+  }
+};
