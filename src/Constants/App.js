ca4516a29f8e8ea5507185444b6229a8aebd51ce
Ejaaz Lakhani <60117208+EjaazLakhani1@users.noreply.github.com>
2022-11-07 21:16:12 -0700

    Ejaaz main (#7)
    * add dependencies
    
    * add basic router
    
    * refactor code
    
    * flow updates

--- src/Constants/App.js
+++ src/Constants/App.js
@@ -1,4 +1,106 @@
 import logo from '../logo.svg'
+import { wordlists } from '../Ejaaz/wordlists';
+
+export const SECTION_CONTENT = {
+  'Ejaaz': {
+    'id': 1,
+    'contents': [
+      wordlists,
+      wordlists,
+      wordlists,
+      wordlists,
+    ]
+  },
+  'Habiba': {
+    'id': 1,
+    'contents': [
+      wordlists,
+      wordlists,
+      wordlists,
+      wordlists,
+    ]
+  },
+  'Colton': {
+    'id': 1,
+    'contents': [
+      wordlists,
+      wordlists,
+      wordlists,
+      wordlists,
+    ]
+  },
+  'Dante': {
+    'id': 1,
+    'contents': [
+      wordlists,
+      wordlists,
+      wordlists,
+      wordlists,
+    ]
+  },
+  'Auric': {
+    'id': 1,
+    'contents': [
+      wordlists,
+      wordlists,
+      wordlists,
+      wordlists,
+    ]
+  },
+}
+
+export const PATHS = {
+  'Ejaaz': {
+    'id': 1,
+    'topic': 'password-cracking',
+    'subtopics': [
+      'wordlists',
+      'intro-to-hashcat',
+      'what-are-hashes',
+      'cracking-hashes',
+    ]
+  },
+  'Habiba': {
+    'id': 2,
+    'topic': 'social-engineering',
+    'subtopics': [
+      'wordlists',
+      'intro-to-hashcat',
+      'what-are-hashes',
+      'cracking-hashes',
+    ]
+  },
+  'Dante': {
+    'id': 3,
+    'topic': 'reverse-engineering',
+    'subtopics': [
+      'wordlists',
+      'intro-to-hashcat',
+      'what-are-hashes',
+      'cracking-hashes',
+    ]
+  },
+  'Colton': {
+    'id': 4,
+    'topic': 'how-to-protect-yourself-on-the-internet',
+    'subtopics': [
+      'wordlists',
+      'intro-to-hashcat',
+      'what-are-hashes',
+      'cracking-hashes',
+    ]
+  },
+  'Auric': {
+    'id': 5,
+    'topic': 'web-exploits',
+    'subtopics': [
+      'wordlists',
+      'intro-to-hashcat',
+      'what-are-hashes',
+      'cracking-hashes',
+    ]
+  }
+}
 
 export const SECTIONS = {
   'Ejaaz': {
@@ -11,7 +113,9 @@ export const SECTIONS = {
       'Intro to Hashcat',
       'What are Hashes',
       'Cracking Hashes',
-    ]
+    ],
+    'paths': PATHS.Ejaaz,
+    'content': SECTION_CONTENT.Ejaaz
   },
   'Habiba': {
     'id': 2,
@@ -23,7 +127,9 @@ export const SECTIONS = {
       'Intro to Hashcat',
       'What are Hashes',
       'Cracking Hashes',
-    ]
+    ],
+    'paths': PATHS,
+    'content': SECTION_CONTENT.Habiba
   },
   'Dante': {
     'id': 3,
@@ -35,11 +141,13 @@ export const SECTIONS = {
       'Intro to Hashcat',
       'What are Hashes',
       'Cracking Hashes',
-    ]
+    ],
+    'paths': PATHS.Dante,
+    'content': SECTION_CONTENT.Dante
   },
   'Colton': {
     'id': 4,
-    'topic': 'How to protect yourself on the internet',
+    'topic': 'How to Protect Yourself on the Internet',
     'author': 'Colton',
     'image': logo,
     'subtopics': [
@@ -47,7 +155,9 @@ export const SECTIONS = {
       'Intro to Hashcat',
       'What are Hashes',
       'Cracking Hashes',
-    ]
+    ],
+    'paths': PATHS.Colton,
+    'content': SECTION_CONTENT.Colton
   },
   'Auric': {
     'id': 5,
@@ -59,6 +169,8 @@ export const SECTIONS = {
       'Intro to Hashcat',
       'What are Hashes',
       'Cracking Hashes',
-    ]
+    ],
+    'paths': PATHS.Auric,
+    'content': SECTION_CONTENT.Auric
   }
 };
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,7 @@
 import './App.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
-import { map } from 'lodash/fp'
 import { Grid, Paper, Box, Button } from '@material-ui/core';
-import { SECTIONS } from './Constants/App';
 import { useNavigate } from 'react-router-dom'
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -15,11 +13,6 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Home = () => {
-  const [page, setPage] = useState({
-    topic: '',
-    author: '',
-    id: 0,
-  });
 
   const navigate = useNavigate();
 
@@ -28,22 +21,28 @@ const Home = () => {
       <>
         <h1>329 Project</h1>
         <Box sx={{ flexGrow: 1 }}>
-          {map((section) => (
-              <Grid container justifyContent="center" alignItems="center" className='Grid'>
-                  <Grid item xs={6}>
-                    <Item>
-                      <h1 className="text">{section.topic}</h1>
-                      <h2 className="text">{section.author}</h2>
-                      <Button onClick={() => {
-                        setPage(section)
-                        navigate(`/lesson/${section.topic}/${section.subtopics[0]}`,{state:{...section}})
-                      }}>
-                        <h2 className="text">Start Lesson</h2>
-                      </Button>
-                    </Item>
-                  </Grid>
+          <Grid container justifyContent="center" alignItems="center" className='Grid'>
+              <Grid item xs={6}>
+                <Item>
+                  <h1 className="text">Lessons</h1>
+                  <Button onClick={() => {
+                    navigate(`/lessons`)
+                  }}>
+                    <h2 className="text">Start Learning</h2>
+                  </Button>
+                </Item>
               </Grid>
-          ), SECTIONS)}
+              <Grid item xs={6}>
+                <Item>
+                  <h1 className="text">CTF</h1>
+                  <Button onClick={() => {
+                    navigate(`/ctf`)
+                  }}>
+                    <h2 className="text">Play CTF</h2>
+                  </Button>
+                </Item>
+              </Grid>
+          </Grid>
         </Box>
       </>
     </div>
